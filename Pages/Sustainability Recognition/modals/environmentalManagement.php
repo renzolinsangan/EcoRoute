72f40d4477a822d4639aedefbfe1eb5a50374ef1
John Renzo Linsangan <PL17431@ymph.net>
2025-09-05 11:54:18 +0800

    Created the Environmental Management Modal, responsive both for Mobile and Desktop.

---
+++ Pages/Sustainability Recognition/modals/environmentalManagement.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EcoRoute - Environmental Management</title>
+    <style>
+        .bi.bi-leaf, .bi.bi-leaf-fill, .bi.bi-check-square-fill {
+            color: #20AB38;
+        }
+
+        .main-card, .leaf-card {
+            border: 2px solid #011901;
+        }
+    </style>
+</head>
+<body>
+    <div class="row p-4">
+        <div class="col-12 col-sm-12 col-md-12 mb-3 ps-0 ps-sm-3 ps-md-3 pe-0 pe-sm-3 pe-md-3">
+            <div class="main-card card">
+                <div class="card-body pt-2 pb-2 d-flex justify-content-start align-items-center">
+                    <i class="bi bi-check-square-fill fs-1"></i>
+                    <span class="fs-sm-4 fs-md-4 ms-1 ms-sm-3 ms-md-3 fw-bold">1 Check</span>
+                    <span class="fs-sm-3 fs-md-3 ms-1 ms-sm-4 ms-md-4 fw-bold">=</span>
+                    <div class="leaf-card card ms-1 ms-sm-4 ms-md-4 pt-1 pb-1 ps-2 pe-2">
+                        <i class="bi bi-leaf-fill fs-3"></i>
+                    </div>
+                    <div class="leaf-container-data d-block pt-3">
+                        <p class="mb-0 ms-1 ms-sm-2 ms-md-2 fw-bold">1 Leaf</p>
+                        <p class="ms-1 ms-sm-2 ms-md-2 fw-medium">(Level 1 Sustainability Badge)</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-12 col-md-12 mb-0 ps-0 ps-sm-4 ps-md-4 pe-0 pe-sm-4 pe-md-4 pb-0">
+            <p class="fw-medium">For this criteria, the tourist spot or establishment should practice these following sustainability practices.</p>
+        </div>
+        <div class="col-12 col-sm-12 col-md-12 mb-3 ps-0 ps-sm-5 ps-md-5 pe-0 pe-sm-5 pe-md-5 pt-0">
+            <div class="photo-proof-container d-block mb-2">
+                <p class="fw-bold mb-0">Photo Proof:</p>
+                <span class="ms-3 me-2"><i class="bi bi-leaf-fill"></i></span><span>Implementation of policies to reduce waste</span><br>
+                <span class="ms-3 me-2"><i class="bi bi-leaf-fill"></i></span><span>Conserve water</span><br>
+                <span class="ms-3 me-2"><i class="bi bi-leaf-fill"></i></span><span>Minimize energy consumption.</span>
+            </div>
+            <div class="photo-proof-container d-block">
+                <p class="fw-bold mb-0">Document Proof (Any of these):</p>
+                <span class="ms-3 me-2"><i class="bi bi-leaf-fill"></i></span><span>Certificate of Non-Coverage (CNC)</span><br>
+                <span class="ms-3 me-2"><i class="bi bi-leaf-fill"></i></span><span>DENR Environmental Complicance Certificate (ECC)</span><br>
+                <span class="ms-4">(if there is no CNC)</span><br>
+                <span class="ms-3 me-2"><i class="bi bi-leaf-fill"></i></span><span>Minimize energy consumption.</span>
+            </div>
+        </div>
+        <div class="col-12 col-sm-12 col-md-12 mb-3 ps-0 ps-sm-4 ps-md-4">
+            <span class="fw-bold">Note:</span>&nbsp;<span>It is not required that everything listed is present in the proof. Thoroughly evaluation is still needed.</span>
+        </div>
+    </div>
+</body>
+</html>
